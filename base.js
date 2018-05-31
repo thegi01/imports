244071c90f994fae4b965d8cd368e09962411de8
thegi01 <deokim77@gmail.com>
2018-05-31 18:11:46 +0900

    change

---
+++ base.js
@@ -0,0 +1,21 @@
+
+/* Include Template */
+// Template Include 
+var insertTempate = function(node, link){
+    link.addEventListener('load', function(){
+        var tp = this.import.querySelector('template');
+        var clone = document.importNode(tp.content, true);
+        node.appendChild(clone);
+        node.outerHTML = node.innerHTML;
+    }, false)
+};
+// link import 생성 및 추가 
+var includes = document.querySelectorAll('[data-include]');
+for(var i=0 ; i<includes.length ; i++){
+    var node = includes[i];
+    var link = document.createElement('link'); 
+    link.rel = 'import';
+    link.href = node.dataset.include +'.html';            
+    document.head.appendChild(link);
+    insertTempate(node, link);
+}
