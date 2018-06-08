c5387c0d3a4533cf8f522b321cb4e5ec9f768891
thegi01 <deokim77@gmail.com>
2018-06-08 17:59:09 +0900

    template css file 추가

--- resources/js/base.js
+++ resources/js/base.js
@@ -1,22 +1,36 @@
 var wrap = document.getElementById('wrap');
 
-/* Include Template */
-// Template Include 
+/* Insert Tempalte Function */
 var insertTempate = function(node, link){
     link.addEventListener('load', function(){
         var tp = this.import.querySelector('template');
+        window.importCssFile(tp);
         var clone = document.importNode(tp.content, true);
         wrap.insertBefore(clone, node);
         node.remove();
     }, false);
 };
-// link import 생성 및 추가 
+
+/* Import Css File Function */
+var importCssFile = function( tp ){
+    var css = tp.dataset.css;
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href= './resources/css/' + css + '.css';
+    document.head.appendChild(link);
+};
+
+
+/* link import 생성 및 Template 추가 */
 var includes = document.querySelectorAll('[data-include]');
 for(var i=0 ; i<includes.length ; i++){
     var node = includes[i];
     var link = document.createElement('link'); 
     link.rel = 'import';
     link.href = node.dataset.include +'.html';            
+    window.insertTempate(node, link);
     document.head.appendChild(link);
-    insertTempate(node, link);
 }
+
+
